13eb17a2079cc4b56af7c9ae0570daf361fd7e8a
JauniKappauni <social.jauni@gmail.com>
2026-01-21 21:47:18 +0100

    init: initialized project with basic structure

---
+++ src/main/java/de/jauni/axscoreboard/AxScoreBoard.java
@@ -0,0 +1,17 @@
+package de.jauni.axscoreboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxScoreBoard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
