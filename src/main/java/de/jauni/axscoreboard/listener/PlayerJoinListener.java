088f275a82d6fff321b12a9a57fe99a49e85e064
JauniKappauni <social.jauni@gmail.com>
2026-01-21 22:44:45 +0100

    feat: add option for custom scoreboard

--- src/main/java/de/jauni/axscoreboard/listener/PlayerJoinListener.java
+++ src/main/java/de/jauni/axscoreboard/listener/PlayerJoinListener.java
@@ -1,5 +1,6 @@
 package de.jauni.axscoreboard.listener;
 
+import de.jauni.axscoreboard.AxScoreBoard;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +9,11 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scoreboard.*;
 
 public class PlayerJoinListener implements Listener {
+    AxScoreBoard reference;
+
+    public PlayerJoinListener(AxScoreBoard reference) {
+        this.reference = reference;
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
@@ -18,9 +24,22 @@ public class PlayerJoinListener implements Listener {
 
         Objective objective = board.registerNewObjective("sidebar", "dummy");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-        objective.setDisplayName("TITLE");
-        objective.getScore("line 1").setScore(1);
-        objective.getScore("line 2").setScore(2);
+        objective.setDisplayName(reference.getValue("scoreboard.title.value"));
+        objective.getScore(reference.getValue("scoreboard.line1.value")).setScore(reference.getScore("scoreboard.line1.score"));
+        objective.getScore(reference.getValue("scoreboard.line2.value")).setScore(reference.getScore("scoreboard.line2.score"));
+        objective.getScore(reference.getValue("scoreboard.line3.value")).setScore(reference.getScore("scoreboard.line3.score"));
+        objective.getScore(reference.getValue("scoreboard.line4.value")).setScore(reference.getScore("scoreboard.line4.score"));
+        objective.getScore(reference.getValue("scoreboard.line5.value")).setScore(reference.getScore("scoreboard.line5.score"));
+        objective.getScore(reference.getValue("scoreboard.line6.value")).setScore(reference.getScore("scoreboard.line6.score"));
+        objective.getScore(reference.getValue("scoreboard.line7.value")).setScore(reference.getScore("scoreboard.line7.score"));
+        objective.getScore(reference.getValue("scoreboard.line8.value")).setScore(reference.getScore("scoreboard.line8.score"));
+        objective.getScore(reference.getValue("scoreboard.line9.value")).setScore(reference.getScore("scoreboard.line9.score"));
+        objective.getScore(reference.getValue("scoreboard.line10.value")).setScore(reference.getScore("scoreboard.line10.score"));
+        objective.getScore(reference.getValue("scoreboard.line11.value")).setScore(reference.getScore("scoreboard.line11.score"));
+        objective.getScore(reference.getValue("scoreboard.line12.value")).setScore(reference.getScore("scoreboard.line12.score"));
+        objective.getScore(reference.getValue("scoreboard.line13.value")).setScore(reference.getScore("scoreboard.line13.score"));
+        objective.getScore(reference.getValue("scoreboard.line14.value")).setScore(reference.getScore("scoreboard.line14.score"));
+        objective.getScore(reference.getValue("scoreboard.line15.value")).setScore(reference.getScore("scoreboard.line15.score"));
 
         p.setScoreboard(board);
     }
